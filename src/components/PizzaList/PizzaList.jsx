19c103fd31d980c56b14cb204ceb86afde6dc1c3
Aubrey Graves <grav0165@gmail.com>
2023-07-13 14:04:33 -0500

    Found cart store to go. Working on conditional display next.

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -15,6 +15,7 @@ import { CardActionArea } from "@mui/material";
 import { useTheme } from '@mui/material/styles';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
 
 // Importing useSelector to pull information from STORE
 import { useSelector } from "react-redux";
@@ -28,6 +29,9 @@ function PizzaList() {
 
     // sourcing pizza list information
     const pizzaList = useSelector(store => store.pizzaList)
+
+    // testing cartlist
+    const cartList = useSelector(store => store.cartList)
 
     // on load, fetching pizza list from server
     useEffect(() => {
@@ -51,43 +55,59 @@ function PizzaList() {
                 console.log('Error in the GET request of PizzaList: ', error);
             })
     }
-    console.log(pizzaList)
+
+    const handleAdd = (pizza) => {
+        dispatch({
+            type: 'ADD_CART',
+            payload: {
+                name: pizza.name,
+                price: pizza.price
+            }
+        })
+    }
+
+console.log('Current cartlist is: ', cartList)
+
+
+
+
     return (
         <div className="pizzaList">
             {
-                pizzaList.map(pizza => 
+                pizzaList.map(pizza =>
                     <div className="single-card" key={pizza.id}>
-                    
-                    <Card sx={{
-                        width: 345,
-                        height: 400,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center'
-                    }} elevation={4}>
-                        <CardActionArea>
-                 
-                            <CardMedia
-                                component="img"
-                                height="140"
-                                image={pizza.image_path}
-                            />
-              
-   
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    {pizza.name}
+                        <Card sx={{
+                            width: 345,
+                            height: 400,
+                            display: 'flex',
+                        }} elevation={4}>
+                            <CardActionArea onClick={()=> handleAdd(pizza)} >
+                                <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image={pizza.image_path}
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {pizza.name}
+                                    </Typography>
+
+                                    <Typography variant="body2" color="text.secondary">
+                                        {pizza.description}
+                                    </Typography>
+                                    <Box sx={{ display: 'flex', alignSelf: 'flex-end', justifyContent: 'flex-end' }}>
+
+                                    </Box>
+                                </CardContent>
+
+                                <Typography variant="body1" color="text.secondary" sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'flex-end' }}>
+                                    ${pizza.price}
                                 </Typography>
-                    
-                                <Typography variant="body2" color="text.secondary">
-                                    {pizza.description}
-                                </Typography>
-                            </CardContent>
-                            <CardContent sx={{alignItems: 'bottom'}}>
-                                <Button size="small">Add</Button>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
+                                {/* <Button size="small">Add</Button> */}
+
+
+                            </CardActionArea>
+                        </Card>
                     </div>
 
                 )
