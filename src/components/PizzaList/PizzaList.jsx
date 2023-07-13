cf34fbd9af7e8f5644baa1bcd87961a02ca9fada
Aubrey Graves <grav0165@gmail.com>
2023-07-13 11:51:43 -0500

    Fixed GET request from server, fixed STORE for store.pizzaList, beginning card CSS

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -56,31 +56,35 @@ function PizzaList() {
         <div className="pizzaList">
             {
                 pizzaList.map(pizza => 
-                    <div className="single-card">
+                    <div className="single-card" key={pizza.id}>
                     
                     <Card sx={{
-                        maxWidth: 345,
+                        width: 345,
+                        height: 400,
                         display: 'flex',
                         flexDirection: 'column',
-                        justifyContent: 'center',
                         alignItems: 'center'
                     }} elevation={4}>
                         <CardActionArea>
+                 
                             <CardMedia
                                 component="img"
                                 height="140"
                                 image={pizza.image_path}
                             />
+              
+   
                             <CardContent>
                                 <Typography gutterBottom variant="h5" component="div">
                                     {pizza.name}
                                 </Typography>
+                    
                                 <Typography variant="body2" color="text.secondary">
                                     {pizza.description}
                                 </Typography>
                             </CardContent>
-                            <CardContent>
-                                <Button size="medium">Add</Button>
+                            <CardContent sx={{alignItems: 'bottom'}}>
+                                <Button size="small">Add</Button>
                             </CardContent>
                         </CardActionArea>
                     </Card>
